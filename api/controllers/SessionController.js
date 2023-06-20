dd546bd6b7d55a25f74151204c62a552876bf152
MuhammedJaseel <muhammedjaseelm@192.168.1.3>
2023-06-20 19:47:23 +0530

    Er ck

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -277,6 +277,7 @@ module.exports = {
     },
     SessionCreate: function(req, res, next) {
         userController.userCreationAutomatically();
+        res.send("Hallow")
 
         function isEmail(email) {
             const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
