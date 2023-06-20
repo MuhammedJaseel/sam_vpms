ee4f00f2995b86b9c6571852fee89c5717235391
MuhammedJaseel <muhammedjaseelm@192.168.1.3>
2023-06-20 19:54:56 +0530

    Er ck

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -351,6 +351,11 @@ module.exports = {
         });
     },
     userCreationAutomatically: function(req, res, next) {
+        console.log('.............................');
+        console.log('.............................');
+        console.log('Hallow Done');
+        console.log('.............................');
+        console.log('.............................');
         User.find(function foundUsers(err, noofuser) {
             if (err) return next(err);
             if (noofuser.length == 0) {
